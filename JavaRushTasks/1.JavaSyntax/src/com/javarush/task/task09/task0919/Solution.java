b8ac30fec9d237daab19e65e1ad9c1030fdc0cee
Andrei RUSU <agrusu@gmail.com>
2017-04-02 15:15:22 +0300

    Am adaugat apropae toate temele din nivelul 9. fara ultimele probleme.

---
+++ JavaRushTasks/1.JavaSyntax/src/com/javarush/task/task09/task0919/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task09.task0919;
+
+/* 
+Деление на ноль
+Создай метод public static void divisionByZero, в котором подели любое число на ноль и выведи на экран результат деления.
+Оберни вызов метода divisionByZero в try..catch. Выведи стек-трейс исключения используя метод exception.printStackTrace()
+
+Требования:
+1. Метод divisionByZero должен содержать операцию деления любого числа на ноль.
+2. Метод divisionByZero должен вызывать System.out.println или System.out.print.
+3. Метод main должен содержать блок try..catch.
+4. Метод main должен отлавливать исключения метода divisionByZero.
+5. Программа должна выводить стек-трейс пойманого исключения.
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            divisionByZero();
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void divisionByZero() throws ArithmeticException {
+        //int x = 3 / 0;
+        System.out.println(3 / 0);
+//        throw new ArithmeticException();
+    }
+}
